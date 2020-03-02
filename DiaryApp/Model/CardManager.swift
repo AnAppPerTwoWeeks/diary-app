6b5dee7a70009a847105429ce2c79642fa918479
mainendp <endp@kakao.com>
2020-03-02 16:35:28 +0900

    realm 에 저장된 카드 데이터를 가져와 화면에 보여준다, 코드 리팩토링

---
+++ DiaryApp/Model/CardManager.swift
@@ -0,0 +1,46 @@
+//
+//  CardManager.swift
+//  DiaryApp
+//
+//  Created by 장창순 on 02/03/2020.
+//  Copyright © 2020 AnAppPerTwoWeeks. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class CardManager {
+    
+    var realm = try! Realm()
+    
+    var cardList: Results<Card>?
+    
+    var getCardListCount: Int {
+        get {
+            cardList?.count ?? 0
+        }
+    }
+    
+    func getCardFromList(_ at: Int) -> Card {
+        return cardList![at]
+    }
+    
+    func saveCard(_ card: Card) {
+        do {
+            try realm.write {
+                realm.add(card)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func loadCardList() {
+        cardList = realm.objects(Card.self)
+    }
+    
+    func addNewCard(title: String, content: String, image: UIImage) {
+        let newCard = Card(title, content, image)
+        saveCard(newCard)
+    }
+}
